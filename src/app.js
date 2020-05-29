5748b21ebaa6e05fc4cc96cc8cd40e6ec74ae718
Jorge Vento <vento.ji@gmail.com>
2020-05-29 20:02:24 +0200

    fixing bugs for deployment

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import configureStore from './store/configureStore';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
+// import 'react-dates/lib/css/_datepicker.css';
 import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
